df27383735d7f6b032f0a66f23a7633b7440f3f5
pauly gallagher <paulygallagher1984@gmail.com>
2023-10-25 11:19:13 +0100

    insert & contians method added for Tree

--- DSA_training/Data_structures/trees.py
+++ DSA_training/Data_structures/trees.py
@@ -12,3 +12,47 @@ class BinarySearchTree:
         # we will update the root node with the insert method,
         # intitializing an empty tree
         self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        # if self.root is None: # not neccesary, while loop handles this 
+        #     return False
+        temp = self.root
+        while temp:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+my_tree = BinarySearchTree()
+my_tree.insert(43)
+my_tree.insert(24)
+my_tree.insert(54)
+my_tree.insert(64)
+
+# print(my_tree.root.value)
+# print (my_tree.root.left.value)
+# print (my_tree.root.right.value)
+# print (my_tree.root.right.right.value)
