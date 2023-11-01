c32124b70b7c8307a40d1d091d06a7bacde7d6e9
pauly gallagher <paulygallagher1984@gmail.com>
2023-11-01 10:39:23 +0000

    sort review

--- DSA_training/Sorted_Algors/bubble_sort.py
+++ DSA_training/Sorted_Algors/bubble_sort.py
@@ -1,15 +1,12 @@
-def bubble_sort(arr1):
-    index_len = len(arr1) -1
-    sorted = False
+def bubble_sort(arr):
+    index_len = len(arr) -1
 
-    while not sorted:
-        sorted = True
-        for i in range(0, index_len):
-            if arr1[i] > arr1[i +1]: #if the next adjacent index is less, then, not sorted
-                sorted = False
-                arr1[i], arr1[i +1] = arr1[i +1], arr1[i] # swapping adjacent indices
-        return arr1 # returning the bubble sorted list/array
+    for i in range(index_len):
+        for j in range(0, index_len - i):
+            if arr[j] > arr[j+1]:
+                arr[j] , arr[j+1] = arr[j +1], arr[j]
+    return arr
 
 
-test_arr = [1, 23, 4, 56, 67, 78, 3]
+test_arr = [109, 23, 4, 56, 67, 78, 3]
 print(bubble_sort(test_arr))
