65920c128fd45e29d90ea7d2692125f960818dff
pauly gallagher <paulygallagher1984@gmail.com>
2023-11-06 14:04:13 +0000

    recursive BST added

--- DSA_training/Data_structures/recursion.py
+++ DSA_training/Data_structures/recursion.py
@@ -69,6 +69,26 @@ class BinarySearchTree:
 
         self.__r_insert(self.root, value)
 
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return False
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+        return current_node
+    
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
+
+
 
 
 my_tree = BinarySearchTree()
