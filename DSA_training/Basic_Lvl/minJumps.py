ed5da4014c12e9c5cd21168c967894b4f4ae2599
pauly gallagher <paulygallagher1984@gmail.com>
2023-11-17 10:16:41 +0000

    minjumps Algor

---
+++ DSA_training/Basic_Lvl/minJumps.py
@@ -0,0 +1,21 @@
+def minJumps(arr, n):
+    jumps = [0 for i in range(n)]
+
+    if n == 0 or arr[0] == 0:
+        return float("inf")
+    
+    jumps[0] = 0
+
+    for i in range(1, n):
+        jumps[i] = float("inf")
+        for j in range(i):
+            
+            if i <= j + arr[j] and jumps[j] != float("inf"):
+                jumps[i] = min(jumps[i], jumps[j] + 1)
+                break
+    print(jumps[n-1])
+
+
+arr= [ 1,3,4,5, 6,9,22]
+n = 7
+minJumps(arr, n)
